8e6e131a2bda589351e0bd1aa76b689b4f406866
Zosyja <zosyja@bk.ru>
2023-03-12 16:16:40 +0300

    added Task001

--- Task.py
+++ Task.py
@@ -5,11 +5,17 @@
 #Выведите минимальное количество монет, которые нужно перевернуть
 
 quantity = int(input('Введите количество монеток на столе: '))
-quantity = []
+list = []
 count_heads = 0
 count_tails = 0
 
 print('Введите монеты: 1=орел, 0=решка')
 for i in range(quantity):
-    q = int(input(f'монета {i} --> '))
-    quantity.append(q)
+    q = int(input(f'монета {i+1} --> '))
+    if (q == 1):
+        count_heads += 1
+    else:
+        count_tails += 1
+    list.append(q)
+
+print(f'Нужно перевернуть {count_heads} монет') if(count_heads < count_tails) else print(f'Нужно перевернуть {count_tails} монет')
